c514fa77786da1f8751e2459488bc449b971f6b2
Hamza <muhammadhamzam1486@gmail.com>
2024-09-01 17:36:15 +0500

    refactor: Update Documents page layout and add table for displaying document data

--- app/rag-knowledge/page.tsx
+++ app/rag-knowledge/page.tsx
@@ -1,19 +1,82 @@
+"use client";
 import WithSidebar from "@/layouts/WithSidebar";
+import { FaEdit, FaRegEdit, FaTrash } from "react-icons/fa";
+import { FaPenToSquare } from "react-icons/fa6";
 
-export default function Knowledge() {
+import { Table, Thead, Tbody, Tr, Td } from "react-super-responsive-table";
+import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+
+export default function Documents() {
   return (
     <WithSidebar>
-      <div className="container mx-auto my-4 p-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full md:w-10/12 lg:w-6/12 bg-white rounded">
-            <div className="relative flex flex-col min-w-0 break-words w-full shadow-lg rounded-lg border-0">
-              <div className="flex-uto px-4 lg:px-10 py-4 pt-0">
-                <h1 className="text-slate-900 text-center mb-8 mt-10 font-bold text-3xl">
-                  Knowledge
-                </h1>
-              </div>
-            </div>
-          </div>
+      <div className="container mx-auto p-4 h-full overflow-auto scrollbar">
+        <div className="flex justify-between items-center">
+          <h1 className="text-slate-900 text-center mt-6 mb-8 font-bold text-3xl md:text-left">
+            Documents
+          </h1>
+
+          <button className="text-white bg-blue-600 px-4 py-2 rounded hover:bg-blue-500">
+            Add Document
+          </button>
+        </div>
+
+        <div className="bg-white p-8 rounded shadow-lg">
+          <Table className="w-full rounded overflow-hidden">
+            <Thead>
+              <Tr className="bg-slate-200">
+                <Td className="p-2">Doc ID</Td>
+                <Td className="p-2">Doc title</Td>
+                <Td className="p-2">Date added</Td>
+                <Td className="p-2">Actions</Td>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td className="p-2">1</Td>
+                <Td className="p-2">Document title</Td>
+                <Td className="p-2">2022-03-15</Td>
+                <Td className="p-2 flex gap-2">
+                  <button className="text-white bg-blue-600 px-4 py-2 rounded hover:bg-blue-500">
+                    <FaPenToSquare />
+                  </button>
+
+                  <button className="text-white bg-red-600 px-4 py-2 rounded hover:bg-red-500">
+                    <FaTrash />
+                  </button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td className="p-2">2</Td>
+                <Td className="p-2">Document title</Td>
+                <Td className="p-2">2022-03-15</Td>
+                <Td className="p-2 flex gap-2">
+                  <button className="text-white bg-blue-600 px-4 py-2 rounded hover:bg-blue-500">
+                    <FaPenToSquare />
+                  </button>
+
+                  <button className="text-white bg-red-600 px-4 py-2 rounded hover:bg-red-500">
+                    <FaTrash />
+                  </button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td className="p-2">3</Td>
+                <Td className="p-2">Document title</Td>
+                <Td className="p-2">2022-03-15</Td>
+                <Td className="p-2 flex gap-2">
+                  <button className="text-white bg-blue-600 px-4 py-2 rounded hover:bg-blue-500">
+                    <FaPenToSquare />
+                  </button>
+
+                  <button className="text-white bg-red-600 px-4 py-2 rounded hover:bg-red-500">
+                    <FaTrash />
+                  </button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          {/* Pagination to be added */}
         </div>
       </div>
     </WithSidebar>

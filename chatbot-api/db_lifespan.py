328ec61992b3c58e8afa40006cd0ff45ef61e9db
Hamza <muhammadhamzam1486@gmail.com>
2024-09-12 21:24:30 +0500

    inject database into the app

---
+++ chatbot-api/db_lifespan.py
@@ -0,0 +1,21 @@
+from contextlib import asynccontextmanager
+from motor.motor_asyncio import AsyncIOMotorClient
+from decouple import config
+from fastapi import FastAPI
+
+
+async def db_lifespan(app: FastAPI):
+    CONNECTION_STRING = config("MONGODB_URI")
+    app.mongodb_client = AsyncIOMotorClient(CONNECTION_STRING)
+    app.database = app.mongodb_client.get_database("chatbot")
+
+    ping_response = await app.mongodb_client.admin.command("ping")
+
+    if int(ping_response["ok"]) != 1:
+        raise Exception("Could not connect to database")
+    else:
+        print("Connected to database")
+
+    yield
+
+    app.mongodb_client.close()

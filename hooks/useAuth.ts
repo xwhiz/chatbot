2d64a30b01a2f2ac7f932406086299d4e883ab0e
Hamza <muhammadhamzam1486@gmail.com>
2024-09-13 14:27:40 +0500

    add a hook to directly read the token from cookies

---
+++ hooks/useAuth.ts
@@ -0,0 +1,23 @@
+import { useCookies } from "next-client-cookies";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { decodeToken } from "@/utils/decodeToken";
+
+export default function useAuth() {
+  const cookies = useCookies();
+  const [payload, setPayload] = useState<any>(null);
+  const token = cookies.get("token");
+  const router = useRouter();
+
+  useEffect(() => {
+    decodeToken(token).then((payload: any) => {
+      setPayload(payload);
+    });
+  }, [token]);
+
+  if (!token) {
+    router.push("/login");
+  }
+
+  return payload;
+}

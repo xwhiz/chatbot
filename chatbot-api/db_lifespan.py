2ec9bcfa7e18263929c9ab4c7ec6bdae594889a4
Hamza <muhammadhamzam1486@gmail.com>
2024-09-22 00:38:53 +0500

    Update requirements .txt

--- chatbot-api/db_lifespan.py
+++ chatbot-api/db_lifespan.py
@@ -8,9 +8,6 @@ from urllib.parse import quote
 async def db_lifespan(app: FastAPI):
     CONNECTION_STRING = config("MONGODB_URI")
 
-    password = quote("123@pakistan")
-    CONNECTION_STRING = f"mongodb+srv://whizhamza:{password}@cluster0.kxfut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
     app.mongodb_client = AsyncIOMotorClient(CONNECTION_STRING)
     app.database = app.mongodb_client.get_database("chatbot")
 

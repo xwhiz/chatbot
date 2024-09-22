4376512fbaf1c3513c42bc4afedbe249a332de0e
Hamza <muhammadhamzam1486@gmail.com>
2024-09-22 16:02:55 +0500

    add a loader until the model starts generating

--- app/page.tsx
+++ app/page.tsx
@@ -192,6 +192,12 @@ export default function Home() {
     <WithSidebar>
       <MessagesFromActiveChatState message={messageState.message} />
 
+      {messageState.isGenerating && (
+        <div className="flex justify-center items-center h-16">
+          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
+        </div>
+      )}
+
       <div className="p-4 border-t">
         <div className="relative">
           <input

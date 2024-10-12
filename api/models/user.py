9945106372b15d94f4a4ae17d659e2ab5f03880f
Hamza <muhammadhamzam1486@gmail.com>
2024-10-12 11:31:59 +0500

    Add accessible_docs field to User model with default value

--- api/models/user.py
+++ api/models/user.py
@@ -7,4 +7,6 @@ class User(BaseModel):
     email: EmailStr = Field(...)
     password: str = Field(...)
     role: str = Field(...)
+    # i need a field called accessible_docs which have the default value of ['all']
+    accessible_docs: list = Field(default_factory=list)
     created_at: datetime = Field(default_factory=datetime.now)

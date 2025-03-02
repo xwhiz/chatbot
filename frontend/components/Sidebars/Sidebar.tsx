9f5d40966d1db4008dc01fc5ee2e125a4f08e545
Hamza <muhammadhamzam1486@gmail.com>
2025-03-02 17:21:21 +0500

    feat: bring the latest chats to the top

--- frontend/components/Sidebars/Sidebar.tsx
+++ frontend/components/Sidebars/Sidebar.tsx
@@ -153,7 +153,7 @@ export function Sidebar() {
               </button>
             </h3>
             <div className="overflow-y-auto">
-              {chatIDs.map((chat) => (
+              {chatIDs.toReversed().map((chat) => (
                 <button
                   key={chat.id}
                   className={`trigger hover:cursor-pointer border-none bg-transparent flex w-full items-center justify-between px-4 py-2 text-gray-700 hover:bg-gray-200 ${

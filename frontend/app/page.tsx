266d982afb90b6999130d46f868962f50593ff42
Hamza <muhammadhamzam1486@gmail.com>
2025-03-15 23:00:33 +0500

    chore: Change loading message

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -328,7 +328,7 @@ export default function Home() {
             <div className="flex justify-center items-center h-16">
               <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
 
-              <p className="ml-2">Retrieving relevent data...</p>
+              <p className="ml-2">Loading...</p>
             </div>
           )}
 

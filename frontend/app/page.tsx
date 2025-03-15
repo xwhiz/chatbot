5d97722a4b8128bf9a819ec53416949e3b3df32c
Hamza <muhammadhamzam1486@gmail.com>
2025-03-15 17:06:53 +0500

    ui: add template for modal of doc selection

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,7 +12,8 @@ import { useUserChatsStore } from "@/stores/userChatsStore";
 import { toast } from "react-toastify";
 import { useActiveChat } from "@/stores/activeChat";
 import { useIsGeneratingStore } from "@/stores/useIsGeneratingStore";
-import { Brain, Square } from "lucide-react";
+import { Brain } from "lucide-react";
+import { Backdrop, Box, Fade, Modal } from "@mui/material";
 
 export default function Home() {
   const router = useRouter();
@@ -37,6 +38,8 @@ export default function Home() {
 
   const [token, sessionInformation] = useAuth();
   const [socket, setSocket] = useState<any>(null);
+  const [docSelectionModalOpen, setDocSelectionModalOpen] =
+    useState<boolean>(false);
 
   const autoResize = () => {
     const textArea: HTMLTextAreaElement | null = textAreaRef.current;
@@ -319,7 +322,7 @@ export default function Home() {
                 <select
                   value={models[selectedModel].modelName}
                   onChange={handleModelChange}
-                  className="p-2 border rounded bg-slate-100"
+                  className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                   disabled={messageState.isGenerating}
                 >
                   {models.map((model) => (
@@ -336,13 +339,14 @@ export default function Home() {
                     className="hidden peer"
                     required
                     value={shouldUseKnowledgeBase as any}
-                    onChange={(e) =>
-                      setShouldUseKnowledgeBase(e.target.checked)
-                    }
+                    onChange={(e) => {
+                      setShouldUseKnowledgeBase(e.target.checked);
+                      if (e.target.checked) setDocSelectionModalOpen(true);
+                    }}
                   />
                   <label
                     htmlFor="knowledgeBase"
-                    className="inline-flex items-center justify-between w-full p-2 text-gray-600 bg-white border-2 border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600 peer-checked:text-black"
+                    className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 inline-flex items-center justify-between w-full p-2 cursor-pointer peer-checked:border-blue-600 peer-checked:text-black"
                   >
                     <div className="flex justify-center items-center gap-1">
                       <Brain className="w-5 h-5" />
@@ -350,6 +354,15 @@ export default function Home() {
                     </div>
                   </label>
                 </div>
+
+                {shouldUseKnowledgeBase && (
+                  <button
+                    className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                    onClick={() => setDocSelectionModalOpen(true)}
+                  >
+                    Select Docs
+                  </button>
+                )}
               </div>
 
               <button
@@ -366,6 +379,58 @@ export default function Home() {
           </div>
         </div>
       </WithSidebar>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={docSelectionModalOpen}
+        onClose={() => setDocSelectionModalOpen(false)}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={docSelectionModalOpen}>
+          <article className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-min bg-white shadow-md max-w-[420px] w-full overflow-hidden rounded">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                Open positions
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                onClick={() => setDocSelectionModalOpen(false)}
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+          </article>
+        </Fade>
+      </Modal>
+
+      {/* <Modal
+        title="Select documents to use as knowledgebase"
+        open={docSelectionModalOpen}
+        handleClose={() => setDocSelectionModalOpen(!docSelectionModalOpen)}
+      /> */}
     </>
   );
 }
